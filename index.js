86300b3c179e81984bce9facfea420991d68eac8
bobbie <bobbiemvelazquez@gmail.com>
2023-10-25 12:00:32 -0500

    css

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// modules and globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// express setting
+app.set('views', _dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 app.use('/places', require('./controllers/places'))
 
@@ -11,12 +15,8 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/', (req, res) => {
-    res.render('places/index')
-})
-
 app.get('*', (req, res) => {
-res.render('error404')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,7 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="styesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
