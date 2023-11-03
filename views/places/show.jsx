fd924a8c7fe5a5338e156adcb375fa0801c19180
bobbie <bobbiemvelazquez@gmail.com>
2023-11-02 21:09:39 -0500

    validations and placeholder pics

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -15,6 +15,21 @@ function show (data) {
     return (
         <Def>
           <main>
+            <div className="row">
+              <div className="col-sm-6">
+                <h3>Located in {data.place.city}, {data.place.state}</h3>
+              </div>
+              <div className="col-sm-6">
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+              
             <h1>Show Page</h1>
 
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
@@ -26,7 +41,8 @@ function show (data) {
                     Delete
                 </button>
             </form> 
-
+            </div>
+            </div>
             </main>
         </Def>
     )
