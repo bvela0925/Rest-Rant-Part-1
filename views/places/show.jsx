09c125754f30266a917ce8fa57d2da1a01199756
bobbie <bobbiemvelazquez@gmail.com>
2023-11-07 21:17:52 -0600

    adjusted rating/stars

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,25 +6,43 @@ function show (data) {
         <h3 className="inactive">
           No comments...yet!
         </h3>
-      )
-      if (data.place.comments.length) {
+        )
+        let rating = (
+          <h3 className="inactive">
+            Not rated...YET!!!
+          </h3>
+        )
+        if (data.place.comments.length) {
+          let sumRatings = data.place.comments.reduce((tot,c) => {
+            return tot + c.stars
+          }, 0)
+          let averageRating = Math.round(sumRatings / data.place.comments.length)
+          let stars = ''
+          for (let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+          }
+          rating = (
+            <h3>
+              {stars} stars
+            </h3>
+          )
         comments = data.place.comments.maps(c => {
           return (
-            <div className="border">
+            <div className="border col-sm-4">
               <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
               <h4>{c.content}</h4>
-              <h3> <strong>- {c.author} </strong></h3>
+              <h3><strong>- {c.author}</strong></h3>
               <h4>Rating: {c.stars}</h4>
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <button type="submit" className="btn btn-danger" value="Delete Comment" >
+                    Delete
+                </button>
+            </form> 
             </div>
           )
         })
       }
-    let rating = (
-        <h3 className="inactive">
-          Not yet rated
-        </h3>
-      )
-    return (
+      return (
         <Def>
           <main>
             <div className="row">
@@ -32,6 +50,11 @@ function show (data) {
                 <h3>Located in {data.place.city}, {data.place.state}</h3>
               </div>
               <div className="col-sm-6">
+                <h2>
+                  Rating
+                </h2>
+                {rating}
+                <br />
                 <h2>
                   Description
                 </h2>
@@ -47,12 +70,6 @@ function show (data) {
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
             </a>     
-
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form> 
             </div>
             </div>
             <h2>Comments</h2>
